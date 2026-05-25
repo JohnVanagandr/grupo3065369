776581f38613d130e4c9fd3e5e6b001050647773
Jhonatan <lehericijhonathandavid@gmail.com>
2026-05-25 17:21:32 -0500

    chore:Se creo la carpeta de api dentro de helpers

--- src/Trash/views/categorias/categoriasControlador.js
+++
@@ -1,9 +0,0 @@
-import { get } from "@/utilidad/solicitudes";
-import { categorias } from "./listarCategorias";
-
-export const categoriasControlador = async () => {
-  const editable = document.querySelector('div > div');
-  const data = await get('categorias');
-
-  editable.innerHTML = categorias(data);
-}
