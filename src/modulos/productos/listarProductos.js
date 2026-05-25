777813c9d9b1658160cf0a7326cc48248e7b64cc
wendy <wendyvanesavera@gmail.com>
2026-05-25 16:46:42 -0500

    Feat: Se agregaron estilos a listarProductos.js

--- src/modulos/productos/listarProductos.js
+++ src/modulos/productos/listarProductos.js
@@ -1,11 +1,25 @@
 export const productos = (data) => {
   return data.map(({ nombre, descripcion }) =>
     `
-    <div>
-      <h2>${nombre}</h2>
-      <p>${descripcion}</p>
-    </div>
-    <Button>Editar Producto</Button>
+    <article 
+    style="background-color:var(--color-white); 
+    margin: 0 auto;
+    margin-top:20px;
+    padding: 16px; 
+    width: 50%;
+    border-radius:8px">
+      <div>
+        <h3>${nombre}</h3>
+        <p>${descripcion}</p>
+      </div>
+      <button class="btn-editar" 
+      style="background-color:var(--color-gray-400); 
+      padding:10px;
+      width: 100%;
+      display: block;
+      margin: 15px auto 0 auto;
+      border-radius:8px;border: none;">Editar Producto</button>
+    </article>
   `
   ).join(' ');// Para quitar las comas
 }
