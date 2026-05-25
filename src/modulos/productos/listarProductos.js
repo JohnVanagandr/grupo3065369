745203a818f36bc62babd522967f62fb57904908
wendy <wendyvanesavera@gmail.com>
2026-05-25 17:03:33 -0500

    Feat: Se agregaron estilos a las card de los productos en listarProductos.js

--- src/modulos/productos/listarProductos.js
+++ src/modulos/productos/listarProductos.js
@@ -1,25 +1,20 @@
 export const productos = (data) => {
   return data.map(({ nombre, descripcion }) =>
     `
-    <article 
-    style="background-color:var(--color-white); 
-    margin: 0 auto;
-    margin-top:20px;
-    padding: 16px; 
-    width: 50%;
-    border-radius:8px">
-      <div>
-        <h3>${nombre}</h3>
-        <p>${descripcion}</p>
-      </div>
-      <button class="btn-editar" 
-      style="background-color:var(--color-gray-400); 
-      padding:10px;
-      width: 100%;
-      display: block;
-      margin: 15px auto 0 auto;
-      border-radius:8px;border: none;">Editar Producto</button>
-    </article>
+    <div class="categoria-card">
+      <div class="categoria-card__body">
+    
+        <div class="categoria-info">
+          <h3 class="categoria-card__titulo">${nombre}</h3>
+          <p class="categoria-card__desc">${descripcion}</p>
+        </div>
+    
+        <button class="categoria-card__btn categoria-card__btn--editar">
+          Editar Producto
+        </button>
+    
+  </div>
+</div>
   `
   ).join(' ');// Para quitar las comas
 }
