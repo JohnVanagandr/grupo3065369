f3a7a10c0123e51893440f0fe2ffbfbd019e4b4d
santiago carrillo <carrilloriverasantiago@gmail.com>
2026-05-25 16:03:59 -0500

    se crea el archivo de asignaciones para el servidor

---
+++ src/utilidad/serviciosAsignacion.js
@@ -0,0 +1,30 @@
+const API_URL = "http://localhost:3000";
+
+
+export async function actualizarCategoriaDelProducto(productoId, nuevaCategoriaId) {
+    try {
+        const respuesta = await fetch(`${API_URL}/productos/${productoId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                categoriaId: Number(nuevaCategoriaId) // Nos aseguramos de que vaya como número
+            })
+        });
+
+        // Si la API responde mal , disparamos un error
+        if (!respuesta.ok) {
+            throw new Error(`Error en el servidor: ${respuesta.status}`);
+        }
+
+        // Si todo sale bien, retornamos los datos actualizados
+        const datosActualizados = await respuesta.json();
+        return datosActualizados;
+
+    } catch (error) {
+        console.error("Hubo un problema en el servicio:", error);
+        // Volvemos a lanzar el error para que el controlador lo sepa y le avise al usuario
+        throw error;
+    }
+}
