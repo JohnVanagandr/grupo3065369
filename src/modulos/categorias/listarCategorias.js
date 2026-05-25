cc914b78d7efb312f2de33176bd148d371b2685f
michaelbenavides <michaelfb803@gmail.com>
2026-05-25 17:08:35 -0500

    se agregan los botones a listas categorias y productos js.

--- src/modulos/categorias/listarCategorias.js
+++ src/modulos/categorias/listarCategorias.js
@@ -1,10 +1,14 @@
 export const categorias = (data) => {
-  return data.map(({ nombre, descripcion }) => `
+  return data.map(({ nombre, descripcion, id}) => `
     <div>
       <h2>${nombre}</h2>
       <p>${descripcion}</p>
     </div>
     <Button>Editar Categoria</Button>
+    <!-- boton eliminar -->
+    <div class="componente_eliminar">
+      <Button class="btn-eliminar" data-id=${id}>Eliminar</Button>
+    </div>
   `
   ).join(' '); // Para poder quitar las comas 
 }
