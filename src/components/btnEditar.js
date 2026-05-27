5afb09767c298a50cf12356f95373a6eb2f8ab3e
Lulizcano.aa@hotmail.com <Lulizcano.aa@hotmail.com>
2026-05-27 11:10:07 -0500

    feat: se añadio matadata de modulo que permite reutilizar el boton en diferentes vistas

--- src/components/btnEditar.js
+++ src/components/btnEditar.js
@@ -1,8 +1,9 @@
-export const btnEditar = (id) => {
+export const btnEditar = (modulo, id) => {
     const button = document.createElement("button");
     button.className = "categoria-card__btn categoria-card__btn--editar";
     button.textContent = "Editar";
     button.dataset.id = id;
+    button.dataset.modulo = modulo;
 
     return button;
 }
