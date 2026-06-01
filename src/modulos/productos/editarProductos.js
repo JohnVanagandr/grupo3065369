35f682efe0ff29b12ff11014541badfc71bd734a
EileenMendoza18 <eileenjml4816@gmail.com>
2026-06-01 16:28:34 -0500

    Fix: Se agrega la funcionalidad de salirse de editar al guardar los cambios de edicion

--- src/modulos/productos/editarProductos.js
+++ src/modulos/productos/editarProductos.js
@@ -1,5 +1,5 @@
 import { get } from '../../helpers/api/index.js' 
-
+import { EditManager } from '../../helpers/editManager.js';
 export async function inicializarFormularioEdicion(id) {
 
     const data = await await get(`productos/${id}`);
@@ -23,11 +23,19 @@ export async function inicializarFormularioEdicion(id) {
 
     // Configuración del botón cancelar
     const btnCancelar = document.getElementById('cancelForm');
+    const btnConfirm= document.getElementById("saveForm");
     if (btnCancelar) {
         btnCancelar.addEventListener('click', (e) => {
             e.preventDefault(); // Evita comportamientos por defecto del botón/formulario
             EditManager.clearEditData();
             window.location.href = '/productos.html'; // O la ruta de tu listado
         });
+    }
+    if(btnConfirm){
+        btnConfirm.addEventListener('click', (e) =>{
+            e.preventDefault();
+            EditManager.clearEditData();
+            window.location.href = '/#/productos'; // O la ruta de tu listado
+        })
     }
 }
